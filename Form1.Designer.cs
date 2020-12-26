0faffd2fa7f91da0c34ced426460b45270e45159
Kashionz <kashionz.nicon.s@gmail.com>
2020-12-26 21:48:53 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Final_Project_Vehicle_Speed_Measurement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.excelToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vehicleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.speedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.excelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.FileListBox = new System.Windows.Forms.ListBox();
+            this.pbImg1 = new System.Windows.Forms.PictureBox();
+            this.pbImg2 = new System.Windows.Forms.PictureBox();
+            this.pbImg3 = new System.Windows.Forms.PictureBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.detectionToolStripMenuItem,
+            this.exportToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1698, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.folderToolStripMenuItem,
+            this.fileToolStripMenuItem1});
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(130, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // folderToolStripMenuItem
+            // 
+            this.folderToolStripMenuItem.Name = "folderToolStripMenuItem";
+            this.folderToolStripMenuItem.Size = new System.Drawing.Size(137, 26);
+            this.folderToolStripMenuItem.Text = "Folder";
+            this.folderToolStripMenuItem.Click += new System.EventHandler(this.folderToolStripMenuItem_Click);
+            // 
+            // fileToolStripMenuItem1
+            // 
+            this.fileToolStripMenuItem1.Name = "fileToolStripMenuItem1";
+            this.fileToolStripMenuItem1.Size = new System.Drawing.Size(137, 26);
+            this.fileToolStripMenuItem1.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.excelToolStripMenuItem1});
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // excelToolStripMenuItem1
+            // 
+            this.excelToolStripMenuItem1.Name = "excelToolStripMenuItem1";
+            this.excelToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.excelToolStripMenuItem1.Text = "Image";
+            this.excelToolStripMenuItem1.Click += new System.EventHandler(this.excelToolStripMenuItem1_Click_1);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(130, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // detectionToolStripMenuItem
+            // 
+            this.detectionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vehicleToolStripMenuItem,
+            this.speedToolStripMenuItem});
+            this.detectionToolStripMenuItem.Name = "detectionToolStripMenuItem";
+            this.detectionToolStripMenuItem.Size = new System.Drawing.Size(89, 26);
+            this.detectionToolStripMenuItem.Text = "Detection";
+            // 
+            // vehicleToolStripMenuItem
+            // 
+            this.vehicleToolStripMenuItem.Name = "vehicleToolStripMenuItem";
+            this.vehicleToolStripMenuItem.Size = new System.Drawing.Size(142, 26);
+            this.vehicleToolStripMenuItem.Text = "Vehicle";
+            this.vehicleToolStripMenuItem.Click += new System.EventHandler(this.vehicleToolStripMenuItem_Click);
+            // 
+            // speedToolStripMenuItem
+            // 
+            this.speedToolStripMenuItem.Name = "speedToolStripMenuItem";
+            this.speedToolStripMenuItem.Size = new System.Drawing.Size(142, 26);
+            this.speedToolStripMenuItem.Text = "Speed";
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.excelToolStripMenuItem});
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(67, 26);
+            this.exportToolStripMenuItem.Text = "Export";
+            // 
+            // excelToolStripMenuItem
+            // 
+            this.excelToolStripMenuItem.Name = "excelToolStripMenuItem";
+            this.excelToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.excelToolStripMenuItem.Text = "Excel";
+            this.excelToolStripMenuItem.Click += new System.EventHandler(this.excelToolStripMenuItem_Click);
+            // 
+            // FileListBox
+            // 
+            this.FileListBox.FormattingEnabled = true;
+            this.FileListBox.ItemHeight = 15;
+            this.FileListBox.Items.AddRange(new object[] {
+            "Waiting for loading...."});
+            this.FileListBox.Location = new System.Drawing.Point(959, 36);
+            this.FileListBox.Name = "FileListBox";
+            this.FileListBox.Size = new System.Drawing.Size(721, 529);
+            this.FileListBox.TabIndex = 1;
+            this.FileListBox.SelectedIndexChanged += new System.EventHandler(this.FileListBox_SelectedIndexChanged);
+            // 
+            // pbImg1
+            // 
+            this.pbImg1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbImg1.Location = new System.Drawing.Point(12, 36);
+            this.pbImg1.Name = "pbImg1";
+            this.pbImg1.Size = new System.Drawing.Size(936, 530);
+            this.pbImg1.TabIndex = 2;
+            this.pbImg1.TabStop = false;
+            // 
+            // pbImg2
+            // 
+            this.pbImg2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbImg2.Location = new System.Drawing.Point(12, 572);
+            this.pbImg2.Name = "pbImg2";
+            this.pbImg2.Size = new System.Drawing.Size(936, 536);
+            this.pbImg2.TabIndex = 3;
+            this.pbImg2.TabStop = false;
+            // 
+            // pbImg3
+            // 
+            this.pbImg3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbImg3.Location = new System.Drawing.Point(959, 572);
+            this.pbImg3.Name = "pbImg3";
+            this.pbImg3.Size = new System.Drawing.Size(721, 536);
+            this.pbImg3.TabIndex = 4;
+            this.pbImg3.TabStop = false;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.RestoreDirectory = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1698, 1121);
+            this.Controls.Add(this.pbImg3);
+            this.Controls.Add(this.pbImg2);
+            this.Controls.Add(this.pbImg1);
+            this.Controls.Add(this.FileListBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "KonPeko";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem folderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem detectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox FileListBox;
+        private System.Windows.Forms.PictureBox pbImg1;
+        private System.Windows.Forms.PictureBox pbImg2;
+        private System.Windows.Forms.PictureBox pbImg3;
+        private System.Windows.Forms.ToolStripMenuItem excelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vehicleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem speedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem excelToolStripMenuItem1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Euresys.Open_eVision_2_12;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Final_Project_Vehicle_Speed_Measurement
+{
+    public partial class Form1 : Form
+    {
+        EImageC24 EC24Image1 = new EImageC24(); //eVision的彩色圖像物件
+        EImageC24 EC24Image2 = new EImageC24(); // EImageC24 instance
+        EImageBW8 EBW8Image1 = new EImageBW8(); //eVision的灰階圖像物件
+        
+        ECannyEdgeDetector cannyEdgeDetector1 = new ECannyEdgeDetector(); // ECannyEdgeDetector instance
+
+        EColorLookup EColorLookup1 = new EColorLookup(); // EColorLookup instance
+
+        EBW8Vector In = new EBW8Vector(); // EBW8Vector instance
+        EBW8Vector Out = new EBW8Vector(); // EBW8Vector instance
+
+        string[] files;
+
+        float ScalingRatio = 0; //Picturebox與原始影像大小的縮放比例
+
+        static Excel.Application Excel_APP1 = new Excel.Application();
+        Excel.Workbook Excel_WB1 = Excel_APP1.Workbooks.Add();
+        Excel.Worksheet Excel_WS1 = new Excel.Worksheet();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void folderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = Application.StartupPath;
+            FileListBox.Items.Clear();
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                files = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.jpg").OrderBy(x => x.Length).ThenBy(x => x).ToArray();
+                foreach (string f in files)
+                {
+                    FileListBox.Items.Add(Path.GetFileName(f));
+                }
+            }
+        }
+
+        private void excelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Excel|*.xlsx";
+            saveFileDialog1.Title = "Save a Excel";
+
+            Excel_WS1 = Excel_WB1.Worksheets[1];
+            Excel_WS1.Name = "Data";
+            Excel_APP1.Cells[1, 1] = "圖片(.jpg)";
+            Excel_APP1.Cells[2, 1] = "Profile下";
+            Excel_APP1.Cells[3, 1] = "Profile上";
+
+            EC24Image1.Load(files[0]);
+
+            /*============================計算scaling ratio============================*/
+            float PictureBoxSizeRatio = (float)pbImg1.Width / pbImg1.Height;
+            float ImageSizeRatio = (float)EC24Image1.Width / EC24Image1.Height;
+            if (ImageSizeRatio > PictureBoxSizeRatio)
+                ScalingRatio = (float)pbImg1.Width / EC24Image1.Width;
+            else
+                ScalingRatio = (float)pbImg1.Height / EC24Image1.Height;
+            /*=========================================================================*/
+
+            for (int i = 0; i < FileListBox.Items.Count; i++)
+            {
+                FileListBox.SelectedIndex = i;
+                FileListBox.Refresh();
+                EC24Image1.Load(files[i]);
+                EC24Image1.Draw(pbImg1.CreateGraphics(), ScalingRatio);
+
+                //EC24Image2.SetSize(EC24Image1);
+                //EasyImage.Oper(EArithmeticLogicOperation.Copy, new EC24(0, 0, 0), EC24Image2);
+
+                //EC24Image1.ColorSystem = EColorSystem.Rgb;
+                //EColorLookup1.ConvertFromRgb(EColorSystem.Yiq);
+                //EColorLookup1.Transform(EC24Image1, EC24Image2);
+
+                //EC24Image2.Draw(pbImg2.CreateGraphics(), ScalingRatio);
+
+                EBW8Image1.SetSize(EC24Image1);
+                EasyImage.Oper(EArithmeticLogicOperation.Copy, new EBW8(0), EBW8Image1);
+                EasyImage.Convert(EC24Image1, EBW8Image1); //轉灰階
+
+                cannyEdgeDetector1.HighThreshold = 1.00f;
+                cannyEdgeDetector1.LowThreshold = 0.20f;
+                cannyEdgeDetector1.Apply(EBW8Image1, EBW8Image1);
+
+                EBW8Image1.Draw(pbImg2.CreateGraphics(), ScalingRatio);
+
+                EasyImage.ImageToLineSegment(EBW8Image1, In, 1000, 1079, 1750, 1079); //設定偵測線在最底部，判斷車子是否準備進來
+                EasyImage.ImageToLineSegment(EBW8Image1, Out, 1000, 0, 1750, 0); //設定偵測線在最頂部，判斷車子是否準備出去
+
+                Excel_APP1.Cells[1, 2 + i] = Path.GetFileNameWithoutExtension(files[i]);
+                Excel_APP1.Cells[2, 2 + i] = getProfileValuesSum(In);
+                Excel_APP1.Cells[3, 2 + i] = getProfileValuesSum(Out);
+
+                Console.WriteLine(files[i]);
+            }
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK && saveFileDialog1.FileName != "")
+                Excel_WB1.SaveAs(saveFileDialog1.FileName);
+
+            Excel_WS1 = null;
+            Excel_WB1.Close();
+            Excel_WB1 = null;
+            Excel_APP1.Quit();
+            Excel_APP1 = null;
+        }
+
+        private double getProfileValuesSum(EBW8Vector eBW8) //Profile線上的值相加起來
+        {
+            double sum = 0;
+            for (int i = 0; i < eBW8.NumElements; i++)
+            {
+                sum += eBW8.GetElement(i).Value;
+            }
+            return sum;
+        }
+
+        private void vehicleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            EC24Image1.Load(files[0]);
+
+            /*============================計算scaling ratio============================*/
+            float PictureBoxSizeRatio = (float)pbImg1.Width / pbImg1.Height;
+            float ImageSizeRatio = (float)EC24Image1.Width / EC24Image1.Height;
+            if (ImageSizeRatio > PictureBoxSizeRatio)
+                ScalingRatio = (float)pbImg1.Width / EC24Image1.Width;
+            else
+                ScalingRatio = (float)pbImg1.Height / EC24Image1.Height;
+            /*=========================================================================*/
+
+            for (int i = 0; i < FileListBox.Items.Count; i++)
+            {
+                FileListBox.SelectedIndex = i;
+                FileListBox.Refresh();
+                EC24Image1.Load(files[i]);
+                EC24Image1.Draw(pbImg1.CreateGraphics(), ScalingRatio);
+
+                //EC24Image2.SetSize(EC24Image1);
+                //EasyImage.Oper(EArithmeticLogicOperation.Copy, new EC24(0, 0, 0), EC24Image2);
+
+                //EC24Image1.ColorSystem = EColorSystem.Rgb;
+                //EColorLookup1.ConvertFromRgb(EColorSystem.Yiq);
+                //EColorLookup1.Transform(EC24Image1, EC24Image2);
+
+                //EC24Image2.Draw(pbImg2.CreateGraphics(), ScalingRatio);
+
+                EBW8Image1.SetSize(EC24Image1);
+                EasyImage.Oper(EArithmeticLogicOperation.Copy, new EBW8(0), EBW8Image1);
+                EasyImage.Convert(EC24Image1, EBW8Image1); //轉灰階
+
+                cannyEdgeDetector1.HighThreshold = 1.00f;
+                cannyEdgeDetector1.LowThreshold = 0.10f;
+                cannyEdgeDetector1.Apply(EBW8Image1, EBW8Image1);
+
+                EBW8Image1.Draw(pbImg2.CreateGraphics(), ScalingRatio);
+
+                Console.WriteLine(files[i]);
+            }
+        }
+
+        private void FileListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            EC24Image1.Load(files[0]);
+
+            /*============================計算scaling ratio============================*/
+            float PictureBoxSizeRatio = (float)pbImg1.Width / pbImg1.Height;
+            float ImageSizeRatio = (float)EC24Image1.Width / EC24Image1.Height;
+            if (ImageSizeRatio > PictureBoxSizeRatio)
+                ScalingRatio = (float)pbImg1.Width / EC24Image1.Width;
+            else
+                ScalingRatio = (float)pbImg1.Height / EC24Image1.Height;
+            /*=========================================================================*/
+
+            EC24Image1.Load(files[FileListBox.SelectedIndex]);
+            EC24Image1.Draw(pbImg1.CreateGraphics(), ScalingRatio);
+
+            //EC24Image2.SetSize(EC24Image1);
+            //EasyImage.Oper(EArithmeticLogicOperation.Copy, new EC24(0, 0, 0), EC24Image2);
+
+            //EC24Image1.ColorSystem = EColorSystem.Rgb;
+            //EColorLookup1.ConvertFromRgb(EColorSystem.Yiq);
+            //EColorLookup1.Transform(EC24Image1, EC24Image2);
+
+            //EC24Image2.Draw(pbImg2.CreateGraphics(), ScalingRatio);
+
+            EBW8Image1.SetSize(EC24Image1);
+            EasyImage.Oper(EArithmeticLogicOperation.Copy, new EBW8(0), EBW8Image1);
+            EasyImage.Convert(EC24Image1, EBW8Image1); //轉灰階
+
+            cannyEdgeDetector1.HighThreshold = 1.00f;
+            cannyEdgeDetector1.LowThreshold = 0.10f;
+            cannyEdgeDetector1.Apply(EBW8Image1, EBW8Image1);
+
+            EBW8Image1.Draw(pbImg2.CreateGraphics(), ScalingRatio);
+
+            Console.WriteLine(files[FileListBox.SelectedIndex]);
+        }
+
+        private void excelToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void excelToolStripMenuItem1_Click_1(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "JPEG|*.jpg";
+            saveFileDialog1.FileName = System.IO.Path.GetFileNameWithoutExtension(files[FileListBox.SelectedIndex]);
+            saveFileDialog1.Title = "Save the Picture";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK && saveFileDialog1.FileName != "")
+                EBW8Image1.Save(saveFileDialog1.FileName, EImageFileType.Jpeg);
+        }
+    }
+}
